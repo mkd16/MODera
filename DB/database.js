185a2389f631cb9bbabdb97ba2722abf9954e16b
mkd16 <mrmohitdaga@gmail.com>
2025-06-26 23:18:35 +0530

    Changed console.log to logger.log in DB connection

--- DB/database.js
+++ DB/database.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose"
 import { MSG_DB } from "../constants.js"
+import { logger } from "../utils/logger.js"
 
 /* DB Connection */
 export const connectDB = async function (){
     try {
         await mongoose.connect(`${process.env.MONGO_URI}/${MSG_DB.DB_NAME}`)
-        console.log('Connected')
+        logger.info(`Database Connected.`)
     } catch (error) {
-        console.log(`${MSG_DB.FAILED_DB_CONNECTION} \n ${error.message}`)
+        logger.error(`${MSG_DB.FAILED_DB_CONNECTION} \n ${error.message}`)
     }
 }
